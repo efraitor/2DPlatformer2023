d66a9423cab21aee0a515ad7257e749ca410cc1e
efraitor_ <efraitor.contact@gmail.com>
2023-02-17 11:03:04 +0100

    UI
    -Added Player Life
    -Added Invincibility

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -7,17 +7,49 @@ public class PlayerHealthController : MonoBehaviour
     //Variables para controlar la vida actual del jugador y el máximo de vida que puede tener
     public int currentHealth, maxHealth;
 
+    //Variables para el contador de tiempo de invencibilidad
+    public float invincibleLength; //Valor que tendrá el contador de tiempo
+    private float invincibleCounter; //Contador de tiempo
+
+    //Referencia del SpriteRenderer del jugador
+    private SpriteRenderer theSR;
+
+    //Hacemos el Singleton de este script
+    public static PlayerHealthController sharedInstance;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //Inicializamos la vida del jugador
         currentHealth = maxHealth;
+        //Obtenemos el SpriteRenderer del jugador
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Comprobamos si el contador de invencibilidad aún no está vacío
+        if (invincibleCounter > 0)
+        {
+            //Le restamos 1 cada segundo a ese contador independientemente del dispositivo que ejecute el juego
+            invincibleCounter -= Time.deltaTime;
+
+            //Cuando el contador haya decrecido hasta 0
+            if (invincibleCounter <= 0)
+            {
+                //Cambiamos el color del sprite, mantenemos el RGB y ponemos la opacidad a tope
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }
     }
 
     //Método para manejar el daño
@@ -35,5 +67,16 @@ public class PlayerHealthController : MonoBehaviour
             //Hacemos desaparecer de momento al jugador
             gameObject.SetActive(false);
         }
+        //Si el jugador ha recibido daño pero no ha muerto
+        else
+        {
+            //Inicializamos el contador de invencibilidad
+            invincibleCounter = invincibleLength;
+            //Cambiamos el color del sprite, mantenemos el RGB y ponemos la opacidad a la mitad
+            theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, .5f);
+        }
+
+        //Actualizamos la UI
+        UIController.sharedInstance.UpdateHealthDisplay();
     }
 }
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Para poder trabajar con elementos de la UI
+
+public class UIController : MonoBehaviour
+{
+    //Referencias a las imágenes de los corazones de la UI
+    public Image heart1, heart2, heart3;
+
+    //Referencias a los sprites que cambiarán al perder o ganar un corazón
+    public Sprite heartFull, heartEmpty;
+
+    //Hacemos el Singleton de este script
+    public static UIController sharedInstance;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Método para actualizar la vida en la UI
+    public void UpdateHealthDisplay()
+    {
+        //En este caso será mejor implementar un Switch ya que depende del valor de la misma variable
+        ////Si la vida del jugador fuera 3
+        //if(PlayerHealthController.sharedInstance.currentHealth == 3)
+        //{
+        //    //Ponemos la imagen de los tres corazones en lleno
+        //    heart1.sprite = heartFull;
+        //    heart2.sprite = heartFull;
+        //    heart3.sprite = heartFull;
+        //}
+
+        //Dependiendo del valor de la vida actual del jugador
+        switch (PlayerHealthController.sharedInstance.currentHealth)
+        {
+            //En el caso en el que la vida actual valga 3
+            case 3:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartFull;
+                //Cerramos el caso
+                break;
+            //En el caso en el que la vida actual valga 2
+            case 2:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartEmpty;
+                //Cerramos el caso
+                break;
+            //En el caso en el que la vida actual valga 1
+            case 1:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                //Cerramos el caso
+                break;
+            //En el caso en el que la vida actual valga 0
+            case 0:
+                heart1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                //Cerramos el caso
+                break;
+            //En el caso por defecto, el jugador estará muerto
+            default:
+                heart1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                //Cerramos el caso
+                break;
+        }
+    }
+}
