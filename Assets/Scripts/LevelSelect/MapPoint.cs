f170e8cf94bd8aeee2e7c2a67551b9608113e110
efraitor_ <efraitor.contact@gmail.com>
2023-03-15 10:33:05 +0100

    Changes
    -Added LevelSelect Scene

---
+++ Assets/Scripts/LevelSelect/MapPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    //Declaramos referencias a los MapPoints adyacentes
+    public MapPoint up, right, down, left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
